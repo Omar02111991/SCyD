03b1c75a07fe4642a36381ad3e5a8a108bb5bfce
Omar02111991 <39389663+Omar02111991@users.noreply.github.com>
2018-05-17 23:36:27 -0500

    IMAGEN DE BOTON DE ELIMINAR
    DE LA TABLA DE USUARIOS REGISTRADOS

--- javascript/tabla.php
+++ javascript/tabla.php
@@ -2,7 +2,8 @@
  require_once 'LIGA3/LIGA.php';
  //BD('localhost', 'root', '', 'base');
  $liga = LIGA('base.usuarios');
- $cols = array('*', 'Acciones'=>'<button class="borrar" data="@[0]">X</button>');
+ $cols = array('*', 'Acciones'=>'<button class="borrar" data="@[0]"><img src="x.png" style="width:19px; height:19px margin top: 95px;
+margin left: 190px;" /></button>');
  
  echo '<label>Filtro: <input id="filtro" name="filtro" /></label>';
  
